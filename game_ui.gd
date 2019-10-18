1b6202ba42679976f36dd78cfb93f35318f988f1
Cristian Espindola <cris.esroj@gmail.com>
2019-10-18 19:46:51 -0300

    modifico score de pasos

--- game_ui.gd
+++ game_ui.gd
@@ -7,9 +7,9 @@ var cantPasos = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global.connect("bronze", self, "bronze")
-	$score.text = "SCORE: " + str(cantPasos)
+	$score.text = "PASOS: " + str(cantPasos)
 	pass
 	
 func bronze(s):
 	cantPasos -= 1
-	$score.text = "SCORE: " + str(cantPasos)
+	$score.text = "PASOS: " + str(cantPasos)
