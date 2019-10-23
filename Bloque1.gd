48470d2b223c8a42774e74b92d20b0be44a222f1
Cristian Espindola <cris.esroj@gmail.com>
2019-10-23 18:13:12 -0300

    modifico movimiento que un bloque movible

--- Bloque1.gd
+++ Bloque1.gd
@@ -1,19 +1,25 @@
 extends Area2D
 
-var pos_nueva_x = 0
-var pos_nueva_y = 0
+#var pos_nueva_x = 0
+#var pos_nueva_y = 0
 
-onready var bloque_nuevo = preload("res://Bloque1.tscn")
+#onready var bloque_nuevo = preload("res://Bloque1.tscn")
+var tileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize() 
 
 func _on_Bloque1_body_entered(body):
+	var sizeCellX = tileMap.cell_size.x
+	var sizeCellY = tileMap.cell_size.y
 	if body.get_name() == "Player":
-		queue_free()
-	var bloque_corrido = bloque_nuevo.instance()
-	pos_nueva_x = randi() % 400 + 1
-	pos_nueva_y = randi() % 250 + 1
-	bloque_corrido.move_local_x(pos_nueva_x)
-	bloque_corrido.move_local_y(pos_nueva_y)
-	get_parent().add_child(bloque_corrido)
+		move_local_x(sizeCellX)
+		move_local_y(sizeCellY)
+#	if body.get_name() == "Player":
+#		queue_free()
+#	var bloque_corrido = bloque_nuevo.instance()
+#	pos_nueva_x = randi() % 400 + 1
+#	pos_nueva_y = randi() % 250 + 1
+#	bloque_corrido.move_local_x(pos_nueva_x)
+#	bloque_corrido.move_local_y(pos_nueva_y)
+#	get_parent().add_child(bloque_corrido)
--- levels/level_1/Level_1.gd
+++ levels/level_1/Level_1.gd
@@ -8,11 +8,13 @@ onready var textGaOv = preload("res://Label/GameOver.tscn")
 onready var player = $Player
 onready var tile = $TileMap
 onready var canvas = $UI/Control
+onready var bloque = $Bloque1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.tileMap = tile
 	player.level_1 = self
 	canvas.player = player
+	bloque.tileMap = tile
 	# Replace with function body.
 	
 
